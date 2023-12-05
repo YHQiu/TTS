a3464ed551ab355143718c32b42d759807b08a84
qiuhongyu <qiuhongyu@taoliweilai.com>
2023-12-05 14:27:30 +0800

    feat

--- ytrains/train_ytts.py
+++ ytrains/train_ytts.py
@@ -6,11 +6,11 @@ from torch import nn
 import os
 from torch.utils.data import DataLoader
 from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.distributed as dist
 
 from train_config import TrainConfig
 from dataset import TextToSpeechDataset
 from ytts_model import YTTS
-import torch.distributed as dist
 
 class TextToSpeechTrainer:
     def __init__(self, model: YTTS, train_loader, val_loader, train_config: TrainConfig):
@@ -27,18 +27,14 @@ class TextToSpeechTrainer:
     def train(self):
         print("开始训练")
         for epoch in range(self.num_epochs):
-
-            print(f"开始训练epoch={epoch}")
-
+            print(f"开始训练 epoch={epoch}")
             self.model.train()
             total_loss = 0.0
 
             for inputs, targets in self.train_loader:
                 self.optimizer.zero_grad()
-                # 输出为多个结果的张量数组
                 outputs = self.model(inputs)
-                # outputs和target都为结果的张量数组
-                loss = self.model.criterion(outputs, targets)
+                loss = self.criterion(outputs, targets)
                 loss.backward()
                 self.optimizer.step()
 
@@ -68,7 +64,7 @@ class TextToSpeechTrainer:
     def load_model(self, path):
         self.model.load_state_dict(torch.load(path))
 
-def main(train_ytts_config):
+def main(train_ytts_config, local_rank):
     # 设置GPU或CPU
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"PyTorch 版本: {torch.__version__}")
@@ -81,13 +77,18 @@ def main(train_ytts_config):
 
     # 从JSON文件加载数据集配置和训练配置
     data_config = config_data['data_config']
-    train_config : TrainConfig = TrainConfig(**config_data['train_config'])
+    train_config = TrainConfig(**config_data['train_config'])
 
-    print(f"加载完成配置文件{train_config} {datetime.datetime.now()}")
+    print(f"加载完成配置文件 {train_config} {datetime.datetime.now()}")
+
+    # 初始化进程组
+    dist.init_process_group(backend='nccl', init_method='tcp://127.0.0.1:12354',
+                            world_size=train_config.world_size, rank=local_rank)
 
     # 加载数据集
     dataset = TextToSpeechDataset(data_config['metadata'], data_config['wavs'])
-    train_loader = DataLoader(dataset, batch_size=train_config.batch_size, shuffle=True)
+    train_sampler = torch.utils.data.distributed.DistributedSampler(dataset)
+    train_loader = DataLoader(dataset, batch_size=train_config.batch_size, sampler=train_sampler)
 
     print(f"加载完成数据集,总计{len(dataset)}条数据待训练 {datetime.datetime.now()}")
 
@@ -95,11 +96,8 @@ def main(train_ytts_config):
     model = YTTS()  # 替换成你的模型
     model.to(device)
 
-    # 初始化DistributedDataParallel，如果启用了分布式训练
-    if torch.cuda.device_count() > 1:
-        # 使用 DistributedDataParallel 包装模型
-        model = DDP(model)
-        print("使用分布式训练 {datetime.datetime}")
+    # 使用 DistributedDataParallel 包装模型
+    model = DDP(model, device_ids=[local_rank])
 
     # 创建训练器
     trainer = TextToSpeechTrainer(model, train_loader, None, train_config)
@@ -112,19 +110,11 @@ def main(train_ytts_config):
     trainer.save_model(model_path)
 
 if __name__ == '__main__':
-
     import argparse
 
     parser = argparse.ArgumentParser(description='Train Text-to-Speech Model')
     parser.add_argument('--train_ytts_config', type=str, default="train_ytts_config.json", help='Path to train YTTS configuration JSON file')
-    parser.add_argument('--local-rank', type=int, default=0)
-    parser.add_argument('--nproc_per_node', type=int, default=torch.cuda.device_count())
-    parser.add_argument('--backend', type=str, default='nccl')
-    parser.add_argument('--world-size', type=int, default=torch.cuda.device_count())
+    parser.add_argument('--local_rank', type=int, default=0)
     args = parser.parse_args()
 
-    # 初始化进程组
-    dist.init_process_group(backend='nccl', init_method='tcp://127.0.0.1:12354',
-                            world_size=args.world_size, rank=args.local_rank)
-
-    main(args.train_ytts_config)
+    main(args.train_ytts_config, args.local_rank)
